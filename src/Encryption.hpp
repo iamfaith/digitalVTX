1d815faa1625564e7bf5fd6a58cea2ff954712a7
consti10 <geierconstantinabc@gmail.com>
2020-12-02 01:55:52 +0100

    XXX tests still passing XXX

--- src/Encryption.hpp
+++ src/Encryption.hpp
@@ -83,6 +83,7 @@ public:
     std::array<uint8_t,crypto_box_SECRETKEYBYTES> rx_secretkey;
     std::array<uint8_t,crypto_box_PUBLICKEYBYTES> tx_publickey;
     std::array<uint8_t,crypto_aead_chacha20poly1305_KEYBYTES> session_key;
+
 };
 
 #endif //ENCRYPTION_HPP
