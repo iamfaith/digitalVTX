3965102abf535c6e870333e676216a247a3d408d
consti10 <geierconstantinabc@gmail.com>
2020-12-05 19:36:11 +0100

    refactoring

---
+++ src/Ieee80211Header.hpp
@@ -0,0 +1,68 @@
+#ifndef __WIFIBROADCAST_IEEE80211_HEADER_HPP__
+#define __WIFIBROADCAST_IEEE80211_HEADER_HPP__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <resolv.h>
+#include <string.h>
+#include <utime.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <pcap.h>
+#include <endian.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <sodium.h>
+#include <endian.h>
+#include <string>
+#include <vector>
+#include <chrono>
+
+extern "C"{
+#include "ExternalSources/ieee80211_radiotap.h"
+};
+
+// Wrapper around the Ieee80211 header (declared as raw array initially)
+class Ieee80211Header{
+public:
+    static constexpr auto SIZE_BYTES=24;
+    //the last byte of the mac address is recycled as a port number
+    static constexpr const auto SRC_MAC_LASTBYTE=15;
+    static constexpr const auto DST_MAC_LASTBYTE=21;
+    static constexpr const auto FRAME_SEQ_LB=22;
+    static constexpr const auto FRAME_SEQ_HB=23;
+    // raw data buffer
+    // unfortunately I do not know what these 'default bytes' mean
+    std::array<uint8_t,SIZE_BYTES> data={
+            0x08, 0x01, 0x00, 0x00,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
+            0x13, 0x22, 0x33, 0x44, 0x55, 0x66,
+            0x00, 0x00,  // seq num << 4 + fragment num
+    };
+    // default constructor
+    Ieee80211Header()=default;
+    // write the port re-using the MAC address (which is unused for broadcast)
+    // write sequence number (not used on rx right now)
+    void writeParams(uint8_t radioPort,uint16_t seqenceNumber){
+        data[SRC_MAC_LASTBYTE] = radioPort;
+        data[DST_MAC_LASTBYTE] = radioPort;
+        data[FRAME_SEQ_LB] = seqenceNumber & 0xff;
+        data[FRAME_SEQ_HB] = (seqenceNumber >> 8) & 0xff;
+    }
+    const uint8_t* getData()const{
+        return data.data();
+    }
+    constexpr std::size_t getSize()const{
+        return data.size();
+    }
+}__attribute__ ((packed));
+static_assert(sizeof(Ieee80211Header) == Ieee80211Header::SIZE_BYTES, "ALWAYS TRUE");
+
+namespace IEEE80211Helper{
+    //static constexpr const auto RX_ANT_MAX=4;
+}
+
+#endif
---
+++ src/RadiotapHeader.hpp
@@ -0,0 +1,109 @@
+#ifndef __WIFIBROADCAST_RADIOTAP_HEADER_HPP__
+#define __WIFIBROADCAST_RADIOTAP_HEADER_HPP__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <resolv.h>
+#include <string.h>
+#include <utime.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <pcap.h>
+#include <endian.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <sodium.h>
+#include <endian.h>
+#include <string>
+#include <vector>
+#include <chrono>
+
+extern "C"{
+#include "ExternalSources/ieee80211_radiotap.h"
+};
+
+// Default is MCS#1 -- QPSK 1/2 40MHz SGI -- 30 Mbit/s
+// MCS_FLAGS = (IEEE80211_RADIOTAP_MCS_BW_40 | IEEE80211_RADIOTAP_MCS_SGI | (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT))
+
+#define MCS_KNOWN (IEEE80211_RADIOTAP_MCS_HAVE_MCS | IEEE80211_RADIOTAP_MCS_HAVE_BW | IEEE80211_RADIOTAP_MCS_HAVE_GI | IEEE80211_RADIOTAP_MCS_HAVE_STBC | IEEE80211_RADIOTAP_MCS_HAVE_FEC)
+
+// Wrapper around the radiotap header (declared as raw array initially)
+class RadiotapHeader{
+public:
+    static constexpr auto SIZE_BYTES=13;
+    // offset of MCS_FLAGS and MCS index
+    static constexpr const auto MCS_FLAGS_OFF=11;
+    static constexpr const auto MCS_IDX_OFF=12;
+    // raw data buffer
+    // unfortunately I do not know what these 'default bytes' mean
+    std::array<uint8_t,SIZE_BYTES> data={
+            0x00, 0x00, // <-- radiotap version
+            0x0d, 0x00, // <- radiotap header length
+            0x00, 0x80, 0x08, 0x00, // <-- radiotap present flags:  RADIOTAP_TX_FLAGS + RADIOTAP_MCS
+            0x08, 0x00,  // RADIOTAP_F_TX_NOACK
+            MCS_KNOWN , 0x00, 0x00 // bitmap, flags, mcs_index
+    };
+    // default constructor
+    RadiotapHeader()=default;
+    // these are the params in use by OpenHD right now
+    struct RadiotapHeaderParams{
+        int bandwidth;
+        int short_gi;
+        int stbc;
+        int ldpc;
+        int mcs_index;
+    };
+    // write the user-selected parameters
+    void writeParams(const RadiotapHeaderParams& params){
+        uint8_t flags = 0;
+        switch(params.bandwidth) {
+            case 20:
+                flags |= IEEE80211_RADIOTAP_MCS_BW_20;
+                break;
+            case 40:
+                flags |= IEEE80211_RADIOTAP_MCS_BW_40;
+                break;
+            default:
+                fprintf(stderr, "Unsupported bandwidth: %d\n", params.bandwidth);
+                exit(1);
+        }
+        if(params.short_gi){
+            flags |= IEEE80211_RADIOTAP_MCS_SGI;
+        }
+        switch(params.stbc) {
+            case 0:
+                break;
+            case 1:
+                flags |= (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT);
+                break;
+            case 2:
+                flags |= (IEEE80211_RADIOTAP_MCS_STBC_2 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT);
+                break;
+            case 3:
+                flags |= (IEEE80211_RADIOTAP_MCS_STBC_3 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT);
+                break;
+            default:
+                fprintf(stderr, "Unsupported STBC type: %d\n",params.stbc);
+                exit(1);
+        }
+        if(params.ldpc){
+            flags |= IEEE80211_RADIOTAP_MCS_FEC_LDPC;
+        }
+        data[MCS_FLAGS_OFF]=flags;
+        data[MCS_IDX_OFF]=params.mcs_index;
+    }
+    void writeParams(int bandwidth,int short_gi,int stbc,int ldpc,int mcs_index){
+        writeParams({bandwidth,short_gi,stbc,ldpc,mcs_index});
+    }
+    const uint8_t* getData()const{
+        return data.data();
+    }
+    constexpr std::size_t getSize()const{
+        return data.size();
+    }
+}__attribute__ ((packed));
+static_assert(sizeof(RadiotapHeader) == RadiotapHeader::SIZE_BYTES, "ALWAYS TRUE");
+
+#endif //__WIFIBROADCAST_RADIOTAP_HEADER_HPP__
