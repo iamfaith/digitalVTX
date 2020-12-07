4b269f495c7f27949e086cbdf28f5f62a6744d76
consti10 <geierconstantinabc@gmail.com>
2020-12-07 18:43:57 +0100

    documentation

--- src/OpenHDStatisticsWriter.hpp
+++ src/OpenHDStatisticsWriter.hpp
@@ -14,11 +14,17 @@ class OpenHDStatisticsWriter{
 public:
     struct Data{
         // all these values are absolute (like done previously in OpenHD)
+        // all received packets
         uint64_t count_p_all=0;
+        // n packets that were received but could not be used (after already filtering for the right port)
         uint64_t count_p_bad=0;
+        // n packets that could not be decrypted
         uint64_t count_p_dec_err=0;
+        // n packets that were successfully decrypted
         uint64_t count_p_dec_ok=0;
+        // n packets that were corrected by FEC
         uint64_t count_p_fec_recovered=0;
+        // n packets that were completely lost though FEC
         uint64_t count_p_lost=0;
         // TODO the rssi stuff ( a bit complicated)
         // since multiple RX are listening on the same wifi card
