381d748e5cecd6a764cea35332e30c3df2cf8068
consti10 <geierconstantinabc@gmail.com>
2020-12-07 16:48:05 +0100

    refactoring

--- src/run_tests.cpp
+++ src/run_tests.cpp
@@ -46,6 +46,107 @@ extern "C"{
 #include "ExternalSources/fec.h"
 }
 
+#include "Helper.hpp"
+namespace TestFEC{
+    // test the FECEncoder / FECDecoder tuple
+    static void testWithoutPacketLoss(const int k, const int n, const std::vector<std::vector<uint8_t>>& testIn){
+        std::cout<<"Test K:"<<k<<" N:"<<n<<" N_PACKETS:"<<testIn.size()<<"\n";
+        FECEncoder encoder(k,n);
+        FECDecoder decoder(k,n);
+        std::vector<std::vector<uint8_t>> testOut;
+
+        const auto cb1=[&decoder](const WBDataPacket &xBlock)mutable {
+            decoder.processPacket(xBlock.header,std::vector<uint8_t>(xBlock.payload,xBlock.payload+xBlock.payloadSize));
+        };
+        const auto cb2=[&testOut](const uint8_t * payload,std::size_t payloadSize)mutable{
+            testOut.emplace_back(payload,payload+payloadSize);
+        };
+        encoder.callback=cb1;
+        decoder.callback=cb2;
+        // If there is no data loss the packets should arrive immediately
+        for(std::size_t i=0;i<testIn.size();i++){
+            const auto& in=testIn[i];
+            encoder.encodePacket(in.data(),in.size());
+            const auto& out=testOut[i];
+            assert(GenericHelper::compareVectors(in,out)==true);
+        }
+    }
+
+    // No packet loss
+    // Fixed packet size
+    static void test(const int k,const int n,const std::size_t N_PACKETS){
+        std::vector<std::vector<uint8_t>> testIn;
+        for(std::size_t i=0;i<N_PACKETS;i++){
+            testIn.push_back(GenericHelper::createRandomDataBuffer(1024));
+        }
+        testWithoutPacketLoss(k, n, testIn);
+    }
+
+    // No packet loss
+    // Dynamic packet size (up to N bytes)
+    static void test2(const int k,const int n,const std::size_t N_PACKETS){
+        std::vector<std::vector<uint8_t>> testIn;
+        for(std::size_t i=0;i<N_PACKETS;i++){
+            const auto size=rand() % MAX_PAYLOAD_SIZE;
+            testIn.push_back(GenericHelper::createRandomDataBuffer(size));
+        }
+        testWithoutPacketLoss(k, n, testIn);
+    }
+
+    // test with packet loss
+    // but only drop one data packet per sequence
+    static void testWithPacketLossButEverythingIsRecoverable(const int k, const int n, const std::vector<std::vector<uint8_t>>& testIn) {
+        assert(testIn.size() % n==0);
+        std::cout << "Test (with packet loss) K:" << k << " N:" << n << " N_PACKETS:" << testIn.size() << "\n";
+        FECEncoder encoder(k, n);
+        FECDecoder decoder(k, n);
+        std::vector <std::vector<uint8_t>> testOut;
+        int packetIdx = 0;
+        const auto cb1 = [&decoder, &packetIdx, n](const WBDataPacket &xBlock)mutable {
+            if (packetIdx % n == 0) {
+                // new sequence, drop one data packet (which FEC can correct for)
+                //std::cout<<"Dropping packet "<<packetIdx<<"\n";
+            }else{
+                decoder.processPacket(xBlock.header,std::vector<uint8_t>(xBlock.payload, xBlock.payload + xBlock.payloadSize));
+            }
+            packetIdx++;
+        };
+        const auto cb2 = [&testOut](const uint8_t *payload, std::size_t payloadSize)mutable {
+            testOut.emplace_back(payload, payload + payloadSize);
+        };
+        encoder.callback = cb1;
+        decoder.callback = cb2;
+        for (std::size_t i = 0; i < testIn.size(); i++) {
+            const auto &in = testIn[i];
+            encoder.encodePacket(in.data(), in.size());
+            // now check if everything already sent arrived
+            // since there is packet loss, you have to wait for the fec to do its magic (latency)
+            if(i % n ==0 && i>0){
+                for(std::size_t j=0;j<i;j++){
+                    const auto &in = testIn[j];
+                    const auto &out = testOut[j];
+                    assert(GenericHelper::compareVectors(in, out) == true);
+                }
+            }
+        }
+        // now check again if everything is still okay
+        for (std::size_t i = 0; i < testIn.size(); i++) {
+            const auto &in = testIn[i];
+            const auto &out = testOut[i];
+            assert(GenericHelper::compareVectors(in, out) == true);
+        }
+    }
+
+    static void test3(const int k,const int n,const std::size_t N_PACKETS){
+        std::vector<std::vector<uint8_t>> testIn;
+        for(std::size_t i=0;i<N_PACKETS;i++){
+            const auto size=rand() % MAX_PAYLOAD_SIZE;
+            testIn.push_back(GenericHelper::createRandomDataBuffer(size));
+        }
+        testWithPacketLossButEverythingIsRecoverable(k, n, testIn);
+    }
+}
+
 
 int main(int argc, char *const *argv){
     std::cout<<"Tests for Wifibroadcast\n";
