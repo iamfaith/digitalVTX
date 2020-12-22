5615e5655d8432251eb0c1d5c78a4ac70d19d087
consti10 <geierconstantinabc@gmail.com>
2020-12-22 19:15:38 +0100

    test on 5.8ghz now, too

--- latencyTesting/startTxAndRxOnMyPc.sh
+++ latencyTesting/startTxAndRxOnMyPc.sh
@@ -4,16 +4,20 @@
 
 #MY_TX="wlp3s0"
 #MY_TX="wlxc4e984126183"
-MY_TX="wlx000f00460445" # ALFA card
+#MY_TX="wlx000f00460445" # ALFA card
 #MY_TX="wlx6cfdb9b2a156" #PW-DN421
 #MY_TX="wlx0018e7bd24db"
+MY_TX="wlx00e0863200b9" #Taobao card
 
-MY_RX="wlxc4e9840e3cbe"
+MY_RX="wlx0018e7bd24db" #Ralink card aliexpress
+#MY_RX="wlxc4e9840e3cbe"
 #MY_RX_SECONDARY="wlxc4e984126183"
 WFB_FOLDER="/home/consti10/Desktop/wifibroadcast"
 
 FEC_K=8
 FEC_N=12
+
+MY_WIFI_CHANNEL=149
 
 sudo rfkill unblock wifi
 #sudo killall ifplugd #stop management of interface
@@ -21,14 +25,14 @@ sudo rfkill unblock wifi
 sudo ifconfig $MY_TX down
 sudo iw dev $MY_TX set monitor otherbss fcsfail
 sudo ifconfig $MY_TX up
-sudo iwconfig $MY_TX channel 13
+sudo iwconfig $MY_TX channel $MY_WIFI_CHANNEL
 #sudo iw dev $MY_TX set channel "6" HT40+
 #sudo iwconfig $MY_TX rts off
 
 sudo ifconfig $MY_RX down
 sudo iw dev $MY_RX set monitor otherbss fcsfail
 sudo ifconfig $MY_RX up
-sudo iwconfig $MY_RX channel 13
+sudo iwconfig $MY_RX channel $MY_WIFI_CHANNEL
 #sudo iwconfig $MY_RX channel "6" HT40+
 
 ## test with multiple RXes
